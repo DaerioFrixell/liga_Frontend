adce6d34ccd5d3da8302e536afbc21e0db0ccd6c
DaerioFrixell <107623124+DaerioFrixell@users.noreply.github.com>
2022-09-16 01:08:36 +0400

    feature/LS-price-05: fixed style

--- src/components/lending/price/Price.jsx
+++ src/components/lending/price/Price.jsx
@@ -4,18 +4,20 @@ import './price.scss';
 export const Price = () => {
   return (
     <div className="price">
-      <h2>цены</h2>
-      <div className="price__less45">
-        <p className="price__less45__title">2000 рублей</p>
-        <p className="price__less45__description">4 урока в месяц по 45 минут</p>
-        <p className="price__less45__title">3700 рублей</p>
-        <p className="price__less45__description">8 уроков в месяц по 45 минут</p>
+      <p className="price__title">цены</p>
+
+      <div className="price__less">
+        <p className="price__less__price">2000 рублей</p>
+        <p className="price__less__description">4 урока в месяц по 45 минут</p>
+        <p className="price__less__price">3700 рублей</p>
+        <p className="price__less__description">8 уроков в месяц по 45 минут</p>
       </div>
-      <div className="price__less60">
-        <p className="price__less60__title">2400 рублей</p>
-        <p className="price__less60__description">4 урока в месяц по 60 минут</p>
-        <p className="price__less60__title">4000 рублей</p>
-        <p className="price__less60__description">8 уроков в месяц по 60 минут</p>
+
+      <div className="price__less">
+        <p className="price__less__price">2400 рублей</p>
+        <p className="price__less__description">4 урока в месяц по 60 минут</p>
+        <p className="price__less__price">4000 рублей</p>
+        <p className="price__less__description">8 уроков в месяц по 60 минут</p>
       </div>
 
     </div>
