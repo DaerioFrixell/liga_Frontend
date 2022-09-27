6e66223e505673a856a38c9cf2f38d11f27bdcd0
DaerioFrixell <107623124+DaerioFrixell@users.noreply.github.com>
2022-09-27 21:30:31 +0400

    feature/LS-09: change AvailableTime

--- src/components/lending/availableTime/AvailableTime.jsx
+++ src/components/lending/availableTime/AvailableTime.jsx
@@ -5,99 +5,49 @@ export const AvailableTime = () => {
   return (
     <div id="availableTimeBlock" className="time">
       <h2 className="time__title">свободное время</h2>
-      <div className="time__day">
-        <p className="time__day__item-d">понедельник</p>
-        <p className="time__day__item">00:00</p>
-        <p className="time__day__item">00:00</p>
-        <p className="time__day__item">00:00</p>
-        <p className="time__day__item">00:00</p>
-        <p className="time__day__item">00:00</p>
-        <p className="time__day__item">00:00</p>
-        <p className="time__day__item">00:00</p>
-        <p className="time__day__item">00:00</p>
-        <p className="time__day__item">00:00</p>
+      <div className="time__list">
+
+        <div className="time__list__day">
+          <p className="time__list__day__item-d">понедельник</p>
+          <p className="time__list__day__item navbl">15:30</p>
+          <p className="time__list__day__item navbl">16:15</p>
+          <p className="time__list__day__item navbl">18:00</p>
+          <p className="time__list__day__item navbl">18:45</p>
+          <p className="time__list__day__item navbl">19:30</p>
+        </div>
+
+        <div className="time__list__day">
+          <p className="time__list__day__item-d">вторник</p>
+          <p className="time__list__day__item avbl">15:30</p>
+          <p className="time__list__day__item avbl">16:15</p>
+          <p className="time__list__day__item navbl">18:00</p>
+          <p className="time__list__day__item navbl">18:45</p>
+          <p className="time__list__day__item avbl">19:30</p>
+        </div>
+
+        <div className="time__list__day">
+          <p className="time__list__day__item-d">среда</p>
+          <p className="time__list__day__item navbl">15:30</p>
+          <p className="time__list__day__item navbl">16:15</p>
+          <p className="time__list__day__item navbl">18:00</p>
+          <p className="time__list__day__item navbl">18:45</p>
+          <p className="time__list__day__item navbl">19:30</p>
+        </div>
+
+        <div className="time__list__day">
+          <p className="time__list__day__item-d">пятница</p>
+          <p className="time__list__day__item navbl">15:30</p>
+          <p className="time__list__day__item navbl">16:15</p>
+          <p className="time__list__day__item navbl">18:00</p>
+          <p className="time__list__day__item avbl">18:45</p>
+          <p className="time__list__day__item avbl">19:30</p>
+        </div>
       </div>
-
-      <div className="time__day">
-        <p className="time__day__item-d">вторник</p>
-        <p className="time__day__item">00:00</p>
-        <p className="time__day__item">00:00</p>
-        <p className="time__day__item">00:00</p>
-        <p className="time__day__item">00:00</p>
-        <p className="time__day__item">00:00</p>
-        <p className="time__day__item">00:00</p>
-        <p className="time__day__item">00:00</p>
-        <p className="time__day__item">00:00</p>
-        <p className="time__day__item">00:00</p>
-      </div>
-
-      <div className="time__day">
-        <p className="time__day__item-d">среда</p>
-        <p className="time__day__item">00:00</p>
-        <p className="time__day__item">00:00</p>
-        <p className="time__day__item">00:00</p>
-        <p className="time__day__item">00:00</p>
-        <p className="time__day__item">00:00</p>
-        <p className="time__day__item">00:00</p>
-        <p className="time__day__item">00:00</p>
-        <p className="time__day__item">00:00</p>
-        <p className="time__day__item">00:00</p>
-      </div>
-
-      <div className="time__day">
-        <p className="time__day__item-d">четверг</p>
-        <p className="time__day__item">00:00</p>
-        <p className="time__day__item">00:00</p>
-        <p className="time__day__item">00:00</p>
-        <p className="time__day__item">00:00</p>
-        <p className="time__day__item">00:00</p>
-        <p className="time__day__item">00:00</p>
-        <p className="time__day__item">00:00</p>
-        <p className="time__day__item">00:00</p>
-        <p className="time__day__item">00:00</p>
-      </div>
-
-      <div className="time__day">
-        <p className="time__day__item-d">пятница</p>
-        <p className="time__day__item">00:00</p>
-        <p className="time__day__item">00:00</p>
-        <p className="time__day__item">00:00</p>
-        <p className="time__day__item">00:00</p>
-        <p className="time__day__item">00:00</p>
-        <p className="time__day__item">00:00</p>
-        <p className="time__day__item">00:00</p>
-        <p className="time__day__item">00:00</p>
-        <p className="time__day__item">00:00</p>
-      </div>
-
-      <div className="time__day">
-        <p className="time__day__item-d">суббота</p>
-        <p className="time__day__item">00:00</p>
-        <p className="time__day__item">00:00</p>
-        <p className="time__day__item">00:00</p>
-        <p className="time__day__item">00:00</p>
-        <p className="time__day__item">00:00</p>
-        <p className="time__day__item">00:00</p>
-        <p className="time__day__item">00:00</p>
-        <p className="time__day__item">00:00</p>
-        <p className="time__day__item">00:00</p>
-      </div>
-
-      <div className="time__day">
-        <p className="time__day__item-d">воскресенье</p>
-        <p className="time__day__item">00:00</p>
-        <p className="time__day__item">00:00</p>
-        <p className="time__day__item">00:00</p>
-        <p className="time__day__item">00:00</p>
-        <p className="time__day__item">00:00</p>
-        <p className="time__day__item">00:00</p>
-        <p className="time__day__item">00:00</p>
-        <p className="time__day__item">00:00</p>
-        <p className="time__day__item">00:00</p>
-      </div>
-
-
 
     </div>
+
+
+
+
   )
 }
