000c31b10ff72f1ef75388a84b8676f96e0d5953
DaerioFrixell <107623124+DaerioFrixell@users.noreply.github.com>
2022-10-10 17:06:02 +0400

    feature/LS-13: rm fails

--- src/components/UI/button/LendingButton.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./lendingButton.scss"
-
-export const LendingButton = ({ nameBtn }) => {
-  return (
-    <button className="lending-button">{nameBtn}</button>
-  )
-}
--- src/components/setting/smoothLink.js
+++
@@ -1,2 +0,0 @@
-export const linkDuration = 700;
-export const linkOffset = -90;
