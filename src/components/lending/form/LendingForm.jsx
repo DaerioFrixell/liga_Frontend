41609b2a61b42413d74474bf266e7fcf59bc1502
DaerioFrixell <107623124+DaerioFrixell@users.noreply.github.com>
2022-10-10 15:23:56 +0400

    feature/LS-13: edit style of form

--- src/components/lending/form/LendingForm.jsx
+++ src/components/lending/form/LendingForm.jsx
@@ -1,32 +1,40 @@
 import React from "react";
-import './ThirdBlock.css';
+import './lendingForm.scss';
 
 export const LendingForm = () => {
-  return (
-    <section id="lendingForm" className="lending-from">
 
+  const handler = e => {
+    e.preventDefault()
+  }
+  return (
+    <section id="lendingForm" className="lf">
       <h2 className="checkMe">Запишись на беспланый пробный урок</h2>
       <form >
-        <div className="form-item">
-          <label className="form-label">имя</label>
-          <input className="form-control" />
+        <div className="lf__item">
+          <label className="lf__item_label">имя</label>
+          <input className="lf__item_input" />
         </div>
-        <div className="form-item">
-          <label className="form-label">номер</label>
-          <input className="form-control" />
+
+        <div className="lf__item">
+          <label className="lf__item_label">номер</label>
+          <input className="lf__item_input" />
         </div>
-        <div className="form-item">
-          <label className="form-label">эл почта</label>
-          <input className="form-control" />
+
+        <div className="lf__item">
+          <label className="lf__item_label">эл почта</label>
+          <input className="lf__item_input" />
         </div>
-        <div className="form-item-input">
-          <button className="form-btn" />
-        </div>
-        <div className="form-item-agree">
+
+        <button
+          className="lf-btn"
+          type="submit"
+          onClick={handler}
+        >записаться</button>
+
+        <div className="lf-agree">
           Соглашаясь, Вы даете согласие на обработку персональных данных
         </div>
       </form>
-
     </section>
   );
 }
